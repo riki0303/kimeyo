7c91e55deaaf1804e68f4ecda3cce97781deb337
小久保力輝 <kokub0303@gmail.com>
2025-12-02 08:52:12 +0900

    style: rubocop対応

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,9 +2,8 @@ class Proposal < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  enum :status, %i(pending approved rejected).index_with(&:to_s), default: :pending, prefix: true
+  enum :status, %i[pending approved rejected].index_with(&:to_s), default: :pending, prefix: true
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 500 }
 end
-
